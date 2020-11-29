509a4597b833d6e6ef11bc151981b38ef56ca3f2
Ivan Kondratyev <zm.fut@bk.ru>
2020-11-29 16:12:12 +0300

    fixFolderName

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,18 +3,18 @@ import api from "../../utils/api";
 import Card from "../Card/Card";
 
 function Main(props) {
-  const [userName, setuserName] = React.useState("");
-  const [userDescription, setuserDescription] = React.useState("");
-  const [userAvatar, setuserAvatar] = React.useState("");
+  const [userName, setUserName] = React.useState("");
+  const [userDescription, setUserDescription] = React.useState("");
+  const [userAvatar, setUserAvatar] = React.useState("");
   const [cards, setCards] = React.useState([]);
 
   React.useEffect(() => {
     api
       .getProfileInfo()
       .then((res) => {
-        setuserName(res.name);
-        setuserDescription(res.about);
-        setuserAvatar(res.avatar);
+        setUserName(res.name);
+        setUserDescription(res.about);
+        setUserAvatar(res.avatar);
       })
       .catch((err) => {
         console.log(err);
